45304c629b46da1f01a34cf1d2e151dec43d3785
Kevin Hilman <khilman@linaro.org>
2013-12-18 08:14:07 -0800

    update_mirror.sh: workaround jenkins bug w/bare repos

--- mirrors/update_mirror.sh
+++ mirrors/update_mirror.sh
@@ -4,3 +4,7 @@ cd $1
 git remote update
 git remote prune next
 git remote prune arm-soc
+
+# Hack for broken Jenkins git plugin which sometimes expects to see a
+# '.git' dir, which doesn't exist in a bare repo
+[[ ! -e .git ]] && ln -s . .git
